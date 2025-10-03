ff84c2db7222c558f26f87abe1a3c697cf97e284
Nathan Frund <nathan@frund.dev>
2025-10-03 18:42:45 -0400

    Cleaned up wargame's damage message.

--- internal/modules/wargame/templates/components/damage.go
+++ internal/modules/wargame/templates/components/damage.go
@@ -8,22 +8,21 @@ import (
 	"maragu.dev/gomponents/html"
 )
 
-// DamageEvent creates a damage event message component for the chat.
-func DamageEvent(targetUnit string, damageAmount int, attackingUnit string) gomponents.Node {
-	// Outer div with HTMX OOB swap
+func DamageEvent(attackingUnit string, damageAmount int, targetUnit string) gomponents.Node {
+	// This is the message content that will be appended to #chat-messages
+	messageContent := html.Div(
+		html.Class("p-2 text-red-500 font-mono border-b border-red-900"),
+		gomponents.Raw(fmt.Sprintf(
+			"<strong>HIT</strong> &mdash; %s deals %d damage to %s!",
+			attackingUnit,
+			damageAmount,
+			targetUnit,
+		)),
+	)
+
+	// Wrap it in a div that tells HTMX to append this to #chat-messages
 	return html.Div(
 		hx.SwapOOB("beforeend:#chat-messages"),
-		// Inner div with styling
-		html.Div(
-			html.Class("p-2 text-red-500 font-mono border-b border-red-900"),
-			// Use gomponents.Raw to render unescaped HTML.
-			// This allows us to use tags like <strong> for formatting.
-			gomponents.Raw(fmt.Sprintf(
-				"<strong>HIT</strong> &mdash; %s deals %d damage to %s!",
-				attackingUnit,
-				damageAmount,
-				targetUnit,
-			)),
-		),
+		messageContent,
 	)
 }
